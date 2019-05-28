7e3db704990d821c1c64931f98a033aa6b28a446
marcosfabrejr <marcosfabrejunior@hotmail.com>
2019-05-28 20:22:31 -0300

    em andamento

--- ListaLigada.java
+++ ListaLigada.java
@@ -1,16 +1,49 @@
 public class ListaLigada {
-    public void adiciona(){
 
+    private Celula primeira;
+    private Celula ultima;
+    private int totalDeElementos;
+
+    public void adiciona(){
+        if(this.totalDeElementos == 0){
+            this.adicionaNoComeco(elemento);
+        }else{
+            Celula nova  = new Celula(elemento);
+            this.ultima.setProxima(nova);
+            this.ultima = nova;
+            this.totalDeElementos++;
+        }
     }
     public void remove(){
 
     }
 
-    public void adicionaNoComeco(){
+    public void adicionaNoComeco(Object elemento){
+        Celula nova  = new Celula(this.primeira, elemento);
+        this.primeira = nova;
 
+        if(this.totalDeElementos == 0){
+            // caso lista vazia
+            this.ultima = this.primeira;
+        }
+
+        this.totalDeElementos++;
     }
+
     public void removeNoComeco(){
-        
+        if(!this.posicaoOcupada(0)){
+            throw new IllegalArgumentException("Posição Inválida");
+        }
+        this.primeira = this.primeira.getProxima();
+        this.totalDeElementos--;
+
+        if(this.totalDeElementos == 0){
+            this.ultima = null;
+        }
+    }
+
+    private boolean posicaoOcupada(int posicao){
+        return posicao >= 0 && posicao < this.totalDeElementos;
     }
 
 }
---
+++ TesteAdicionaNoComeco.java
@@ -0,0 +1,13 @@
+/**
+ * TesteAdicionaNoComeco
+ */
+public class TesteAdicionaNoComeco {
+
+    public static void main(String[] args){
+        ListaLigada lista = new ListaLigada();
+
+        lista.adicionaNoComeco("Gabriel");
+        lista.adicionaNoComeco("Rodrigo");
+    }
+    
+}
